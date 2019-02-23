c5a4260932f6c04678de0d064670430ccd70ff4a
Braedon <braedongough@gmail.com>
2019-02-23 14:34:53 +0100

    refactored reduce to use handleActions from redux-actions

--- src/redux/modules/wizard.js
+++ src/redux/modules/wizard.js
@@ -1,4 +1,4 @@
-import { createActions } from "redux-actions";
+import { createActions, handleActions, combineActions } from "redux-actions";
 
 //!const REGISTER_STEP_COUNT = "wizard/REGISTER_STEP_COUNT"; <- what is this actions meant to be for?
 export const GET_STEP_COUNT = "wizard/GET_STEP_COUNT";
@@ -13,30 +13,26 @@ export const {
   [PREV]: () => ({ modifier: -1 }),
 });
 
-const initialState = {
-  numberOfSteps: 0,
-  currentStep: 1,
-};
-
-const wizardReducer = (state = initialState, { type, payload }) => {
-  switch (type) {
-    case GET_STEP_COUNT:
-      return {
-        ...state,
-        numberOfSteps: payload.numberOfSteps,
-      };
-    case NEXT:
-    case PREV:
-      const nextStep = state.currentStep + payload.modifier;
+const wizardReducer = handleActions(
+  {
+    [GET_STEP_COUNT]: (state, { payload }) => ({
+      ...state,
+      numberOfSteps: payload.numberOfSteps,
+    }),
+    [combineActions(next, prev)]: (state, { payload: { modifier } }) => {
+      const nextStep = state.currentStep + modifier;
       const numberOfSteps = state.numberOfSteps;
       const checkNumberOfSteps = 0 < nextStep && nextStep <= numberOfSteps;
       return {
         ...state,
         currentStep: checkNumberOfSteps ? nextStep : state.currentStep,
       };
-    default:
-      return state;
+    },
+  },
+  {
+    numberOfSteps: 0,
+    currentStep: 1,
   }
-};
+);
 
 export default wizardReducer;
