bd70d17ad9b3e846de1dbaf735c0049c672c52c7
Braedon <braedongough@gmail.com>
2019-02-27 08:45:17 +0100

    refactored wizard component to decouple from Form

--- src/Components/Questionnaire/Questionnaire.js
+++ src/Components/Questionnaire/Questionnaire.js
@@ -1,38 +1,123 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Field } from "formik";
+import { Form, Formik, Field } from "formik";
+import generatePDF from "../../utils/generatePDF";
+import { wizardModule, policyModule } from "../../redux/modules";
 import Header from "../Header/Header";
+import Nav from "./FormNav";
 import Wizard from "../Wizard/Wizard";
 import Step from "../Wizard/Step";
 
-const Questionnaire = ({ currentStep }) => {
+const Questionnaire = ({ currentStep, next, numberOfSteps }) => {
+  const handleSubmit = (values, actions) => {
+    const isLastPage = currentStep === numberOfSteps;
+
+    if (!isLastPage) {
+      next(values);
+      actions.setSubmitting(false);
+    } else {
+      const text = JSON.stringify(values, null, 2);
+
+      generatePDF(text);
+      alert(text);
+      actions.setSubmitting(false);
+    }
+  };
+
   return (
     <div>
       <Header textLabel={`Step ${currentStep}`} textTitle="Questionnaire" />
-      <Wizard>
-        <Step>
-          <Field type="text" name="companyName" placeholder="Company Name" />
-        </Step>
-        <Step>
-          <Field
-            type="number"
-            name="numberOfEmployees"
-            placeholder="Number of Employees"
-          />
-        </Step>
-        <Step>
-          <Field type="number" name="budget" placeholder="budget" />
-        </Step>
-        <Step>
-          <Field type="email" name="email" placeholder="email" />
-        </Step>
-      </Wizard>
+      <Formik
+        initialValues={{
+          companyName: "",
+          numberOfEmployees: "",
+          budget: "",
+          email: "",
+        }}
+        onSubmit={handleSubmit}
+        render={() => (
+          <Form>
+            <Wizard>
+              <Step>
+                <Field
+                  type="text"
+                  name="companyName"
+                  placeholder="Company Name"
+                />
+              </Step>
+              <Step>
+                <Field
+                  type="number"
+                  name="numberOfEmployees"
+                  placeholder="Number of Employees"
+                />
+              </Step>
+              <Step>
+                <Field type="number" name="budget" placeholder="budget" />
+              </Step>
+              <Step>
+                <Field type="email" name="email" placeholder="email" />
+              </Step>
+            </Wizard>
+            <Nav />
+          </Form>
+        )}
+      />
     </div>
   );
 };
 
 const mapStateToProps = ({ wizard }) => ({
   currentStep: wizard.currentStep,
+  numberOfSteps: wizard.numberOfSteps,
 });
 
-export default connect(mapStateToProps)(Questionnaire);
+const mapDispatchToProps = dispatch => ({
+  next: values => {
+    dispatch(policyModule.next(values));
+    dispatch(wizardModule.next());
+  },
+});
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Questionnaire);
+
+/* 
+      <Formik
+        initialValues={{
+          companyName: "",
+          numberOfEmployees: "",
+          budget: "",
+          email: "",
+        }}
+        onSubmit={handleSubmit}
+        render={() => (
+          <Form>
+            <Wizard>
+              <Step>
+                <Field
+                  type="text"
+                  name="companyName"
+                  placeholder="Company Name"
+                />
+              </Step>
+              <Step>
+                <Field
+                  type="number"
+                  name="numberOfEmployees"
+                  placeholder="Number of Employees"
+                />
+              </Step>
+              <Step>
+                <Field type="number" name="budget" placeholder="budget" />
+              </Step>
+              <Step>
+                <Field type="email" name="email" placeholder="email" />
+              </Step>
+            </Wizard>
+            <Nav />
+          </Form>
+        )}
+      />
+*/
--- src/Components/Wizard/Wizard.js
+++ src/Components/Wizard/Wizard.js
@@ -1,71 +1,24 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import { Formik, Form } from "formik";
-import { wizardModule, policyModule } from "../../redux/modules";
-import generatePDF from "../../utils/generatePDF";
-import Nav from "./FormNav";
+import { wizardModule } from "../../redux/modules";
 
-const Wizard = ({
-  children,
-  getStepCount,
-  currentStep,
-  numberOfSteps,
-  policy,
-  next,
-}) => {
+const Wizard = ({ children, getStepCount, currentStep, numberOfSteps }) => {
   const filteredChildren = () =>
     React.Children.toArray(children).filter(
       child => child.type.name === "Step"
     );
-
-  const formData = () => policy;
-
-  const handleSubmit = (values, actions) => {
-    const isLastPage = currentStep === numberOfSteps;
-    if (!isLastPage) {
-      next(values);
-      actions.setSubmitting(false);
-    } else {
-      const text = JSON.stringify(values, null, 2);
-      generatePDF(text);
-      setTimeout(() => {
-        alert(text);
-        actions.setSubmitting(false);
-      }, 500);
-    }
-  };
 
   useEffect(() => {
     if (!numberOfSteps) {
       getStepCount(React.Children.count(filteredChildren()));
     }
   });
-
-  return (
-    <Formik
-      initialValues={
-        {
-          // name: ''
-          // Braedon check here
-        }
-      }
-      onSubmit={values => {
-        // createpolicy(values) <= redux action
-
-        return console.log("values", values);
-      }}
-      render={({ values }) => (
-        <Form>
-          {React.Children.map(filteredChildren(), (child, index) =>
-            React.cloneElement(child, {
-              isActive: currentStep === index + 1,
-            })
-          )}
-          <Nav />
-        </Form>
-      )}
-    />
+  const steps = React.Children.map(filteredChildren(), (child, index) =>
+    React.cloneElement(child, {
+      isActive: currentStep === index + 1,
+    })
   );
+  return steps;
 };
 
 const mapStateToProps = ({ wizard, policy }) => ({
@@ -77,8 +30,7 @@ const mapStateToProps = ({ wizard, policy }) => ({
 const mapDispatchToProps = dispatch => ({
   getStepCount: numberOfSteps =>
     dispatch(wizardModule.getStepCount(numberOfSteps)),
-  next: values => {
-    dispatch(policyModule.next(values));
+  next: () => {
     dispatch(wizardModule.next());
   },
 });
